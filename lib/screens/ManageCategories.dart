c30fefa14911e7564d9f51c8479fd63e80344b2f
Raza <mailrazaiqbal@gmail.com>
2021-06-08 21:54:26 +0530

    added manage categories page

---
+++ lib/screens/ManageCategories.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:todo/services/firebase.dart';
+import 'package:todo/widgets/AppBarWidget.dart';
+
+class ManageCategories extends StatefulWidget {
+  @override
+  _ManageCategoriesState createState() => _ManageCategoriesState();
+}
+
+class _ManageCategoriesState extends State<ManageCategories> {
+  final _formKey = GlobalKey<FormState>();
+  String dropdownValue = 'All';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWidget(
+        title: RichText(
+          text: TextSpan(
+            children: [
+              WidgetSpan(
+                child: Icon(Icons.note_add_rounded, size: 20),
+              ),
+              TextSpan(
+                  text: " Add Todo",
+                  style: TextStyle(color: Colors.black, fontSize: 18)),
+            ],
+          ),
+        ),
+      ),
+      body: Container(
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 20,
+        ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Select Category'),
+              DropdownButtonFormField(
+                value: dropdownValue,
+                onChanged: (String newValue) {
+                  setState(() {
+                    dropdownValue = newValue;
+                  });
+                },
+                items: <String>['All', 'Work', 'Personal', 'Home']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text('Todo'),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Get groceries from supermarket.',
+                  // labelText: 'Todo',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a task';
+                  }
+                },
+                maxLines: null,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    FirebaseServices.createTodo();
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Processing Data')));
+                  }
+                },
+                child: Text('Add'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
